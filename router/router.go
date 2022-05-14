cc1fb6a8f5e7c261182b38c9e112cf18ff22a670
dema <dema@dema.com>
2022-05-14 16:57:27 +0800

    second commit

--- router/router.go
+++ router/router.go
@@ -13,7 +13,7 @@ func SetUpRouter() *gin.Engine {
 	// 告诉gin框架模板文件需要的静态文件在哪
 	r.Static("/resource/", "./public/resource/")
 	// 对应模板中的函数
-	r.SetFuncMap(template.FuncMap{"isODD": IsODD, "getNextName": GetNextName, "date": Date})
+	r.SetFuncMap(template.FuncMap{"isODD": IsODD, "getNextName": GetNextName, "date": Date, "dateDay": DateDay})
 	// 告诉gin框架去哪个文件夹去找模板文件
 	home := "template/home.html"
 	footer := "template/layout/footer.html"
